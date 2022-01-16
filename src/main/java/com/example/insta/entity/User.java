dc8357ab5034e9569263891f380acb111d540ef9
Damir <shakirov-damir@mail.ru>
2022-01-16 21:09:34 +0300

    test

---
+++ src/main/java/com/example/insta/entity/User.java
@@ -0,0 +1,102 @@
+package com.example.insta.entity;
+
+import com.example.insta.entity.enums.ERole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Data // @ToString, @EqualsAndHashCode, @Getter / @Setter, @RequiredArgsConstructor
+@Entity
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, updatable = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(columnDefinition = "text")
+    private String bio;
+
+    @Column(length = 3000)
+    private String password;
+
+    @ElementCollection(targetClass = ERole.class)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    private Set<ERole> roles = new HashSet<>();
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "user", orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+    @JsonFormat(pattern = "yyyy-mm-dd HH:mm:ss")
+    @Column(updatable = false)
+    private LocalDateTime createdDate; // когда был создан объект
+
+    @Transient
+    /* @Transient — указывает, что свойство не нужно записывать. Значения под этой аннотацией не записываются в базу данных
+    (также не участвуют в сериализации). static и final переменные экземпляра всегда transient. */
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public User(){};
+
+    @PrePersist // вызывается как только инициирован вызов persist() и исполняется перед остальными действиями.
+    protected void onCreateDate(){
+        this.createdDate = LocalDateTime.now();
+    }
+
+    public User(Long id,
+                String username,
+                String email,
+                String password,
+                Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    /**
+     * Security
+     */
+
+    @Override
+    public String getPassword(){
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
