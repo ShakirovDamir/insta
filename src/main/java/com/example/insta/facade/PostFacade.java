cd1cbea85e7ee087872c39b5cbf8154a1057a913
Damir <shakirov-damir@mail.ru>
2022-01-30 22:23:17 +0300

    Add RestController

---
+++ src/main/java/com/example/insta/facade/PostFacade.java
@@ -0,0 +1,23 @@
+package com.example.insta.facade;
+
+import com.example.insta.dto.PostDTO;
+import com.example.insta.entity.Post;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostFacade {
+
+    public PostDTO postToPostDTO(Post post) {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setUsername(post.getUser().getUsername());
+        postDTO.setId(post.getId());
+        postDTO.setCaption(post.getCaption());
+        postDTO.setLikes(post.getLikes());
+        postDTO.setUsersLiked(post.getLikedUsers());
+        postDTO.setLocation(post.getLocation());
+        postDTO.setTitle(post.getTitle());
+
+        return postDTO;
+    }
+
+}
